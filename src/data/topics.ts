328f1ea9176e552c0106507b680aee68ca355832
shiojojo <shiroshi309@gmail.com>
2025-08-19 19:25:50 +0900

    feat: update imageUrl for the first topic to use Blogger URL instead of Google Drive

--- src/data/topics.ts
+++ src/data/topics.ts
@@ -7,7 +7,7 @@ export const topics: Topic[] = Array.from({ length: 100 }, (_, i) => {
   return {
     topic: `お題 ${idx} - ダミーの質問`,
     ...(i === 0
-      ? { imageUrl: 'https://drive.google.com/uc?export=view&id=1QM5tYApmGXt3qZdvsI2-dwS2gmgbINK2' }
+      ? { imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEieks99OzkiAothfzTz7FbtakQMfXVPQDL6eUDMJfv_2ghB5xB0gUYsA5n-2YUHe5Adevn9YrfUjswiDQneXg1Q0uzEjIu3R9G-DJ7xvxi6nbj-XiNWool1RV3lRjy3-zKFGPySzfgQxHGw/s650/animal_black_sheep_hitsuji.png' }
       : {}),
     answers: Array.from({ length: 20 }, (_, a) => ({
       id: a + 1,
